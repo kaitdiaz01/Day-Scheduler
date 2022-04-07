f76f5ff341b30895ea495ae3594ab48fc55d2e1d
Kaitlyn Diaz <katediaz011@gmail.com>
2022-04-07 18:54:48 -0400

    added conditions for timeblocks to display color

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,19 +1,17 @@
-// set timer in the header
-// var time = moment().format("MMMM Do YYYY")
-
-var time = moment().format("MMMM Do YYYY, h:mm:ss a");
-$("#currentDay").text(time);
-
-// update in real time
-var update = function () {
-  document.getElementById("currentDay").innerHTML = moment().format(
-    "MMMM Do YYYY, h:mm:ss a"
-  );
-};
-setInterval(update, 1000);
-
-//start a function using jQuery syntax
 $(document).ready(function (event) {
+  var time = moment().format("MMMM Do YYYY, h:mm:ss a");
+  $("#currentDay").text(time);
+
+  // update in real time
+  var update = function () {
+    document.getElementById("currentDay").innerHTML = moment().format(
+      "MMMM Do YYYY, h:mm:ss a"
+    );
+  };
+  setInterval(update, 1000);
+
+  //start a function using jQuery syntax
+
   //listen for savebtn clicks (when we type in the text field do something to save that)
   $(".saveBtn").on("click", function () {
     var value = $(this).siblings(".information").val();
@@ -26,32 +24,39 @@ $(document).ready(function (event) {
     var currentHour = moment().hours();
     //create a variable that holds the current time value
     $(".time-block").each(function () {
-    //   create something to loop over the time blocks
-      var blockTime = $("div.hour").text();
-    // want to tell it for each class of time block look at div with class of hour and get the value so in the 
-    // if statement i can compare it to currentHour 
-    //how do i seperate the words from the number
+      //   create something to loop over the time blocks
+      // want to tell it for each class of time block look at div with class of hour and get the value so in the if statement i can compare it to currentHour
+      // had to parseInt to turn the string into an interger
+      var blockTime = parseInt($(this).attr("id").split("-")[1]);
+      console.log(blockTime);
+      if (currentHour > blockTime) {
+        //add conditonal to change class
+        $(this).addClass("past");
+      } else if (currentHour >= blockTime) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("future");
+      }
+      //how do i seperate the words from the number
     });
-    if (blockTime < currentHour) {
-    
-    } else if (currentHour >= blockTime && currentHour < blockTime) {
-    }
   }
 
   colorUpdate();
 
   // var  interval = setInterval(colorUpdate, 15000);
   //need saved data from local storage
-  $("#hour-nine .information").val(localStorage.getItem("#hour-nine"));
-  $("#hour-ten .information").val(localStorage.getItem("#hour-ten"));
-  $("#hour-eleven .information").val(localStorage.getItem("#hour-eleven"));
-  $("#hour-twelve .information").val(localStorage.getItem("#hour-twelve"));
-  $("#hour-thirteen .information").val(localStorage.getItem("#hour-thirteen"));
-  $("#hour-fourteen .information").val(localStorage.getItem("#hour-fourteen"));
-  $("#hour-fifteen .information").val(localStorage.getItem("#hour-fifteen"));
-  $("#hour-sixteen .information").val(localStorage.getItem("#hour-sixteen"));
-  $("#hour-seventeen .information").val(localStorage.getItem("#hour-seventeen"));
-  //why is information deleting from page and not saving even though it is in local storage
+  $("#hour-9 .information").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .information").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .information").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .information").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .information").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .information").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .information").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .information").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .information").val(localStorage.getItem("hour-17"));
+  $("#hour-18 .information").val(localStorage.getItem("hour-18"));
+  $("#hour-19 .information").val(localStorage.getItem("hour-19"));
+  $("#hour-20 .information").val(localStorage.getItem("hour-20"));
 
   $("#currentDay").text(moment().format("MMMM Do YYYY"));
-})
+});
